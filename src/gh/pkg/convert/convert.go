a3b47c6d224954edff63ef290c8dba4b4d5b97fe
117503445 <t117503445@gmail.com>
2024-07-13 23:29:00 +0800

    update ConvertToNewImage
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/gh/pkg/convert/convert.go
+++ src/gh/pkg/convert/convert.go
@@ -1,44 +1,64 @@
 package convert
 
 import (
-	"errors"
+	// "errors"
 	"strings"
 
 	dockerparser "github.com/novln/docker-parser"
 )
 
 const (
-	NEW_REGISTRY = "registry.cn-hangzhou.aliyuncs.com"
-	NEW_USERNAME = "117503445-mirror"
+	NEW_REGISTRY  = "registry.cn-hangzhou.aliyuncs.com"
+	NEW_SHORTNAME = "117503445-mirror/sync"
 )
 
 func ConvertToNewImage(image string) (string, error) {
+	image = strings.TrimSpace(image)
+
 	reference, err := dockerparser.Parse(image)
 	if err != nil {
 		return "", err
 	}
 
-	splits := strings.Split(reference.ShortName(), "/")
-	if len(splits) <= 1 {
-		return "", errors.New("image with len(shortName.split(\"/\")) <= 1")
+	newTag := reference.Registry() + "." + reference.Name()
+
+	chars := []string{"/", ":", "@"}
+	for _, char := range chars {
+		newTag = strings.ReplaceAll(newTag, char, ".")
 	}
 
-	// if len(splits) != 2 {
-	// 	log.Warn().Str("image", image).Msg("image with len(shortName.split(\"/\")) > 2")
-	// }
-
-	username := splits[0]
-	name := strings.Join(splits[1:], "/")
-
-	tag := reference.Tag()
-	var suffix string
-	if strings.HasPrefix(tag, "sha256:") {
-		suffix = "@" + tag
-	} else {
-		suffix = ":" + tag
-	}
-
-	newImage := NEW_REGISTRY + "/" + NEW_USERNAME + "/" + reference.Registry() + "." + username + "." + name + suffix
+	newImage := NEW_REGISTRY + "/" + NEW_SHORTNAME + ":" + newTag
 
 	return newImage, nil
 }
+
+// func ConvertToNewImage(image string) (string, error) {
+// 	reference, err := dockerparser.Parse(image)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	splits := strings.Split(reference.ShortName(), "/")
+// 	if len(splits) <= 1 {
+// 		return "", errors.New("image with len(shortName.split(\"/\")) <= 1")
+// 	}
+
+// 	// if len(splits) != 2 {
+// 	// 	log.Warn().Str("image", image).Msg("image with len(shortName.split(\"/\")) > 2")
+// 	// }
+
+// 	username := splits[0]
+// 	name := strings.Join(splits[1:], "/")
+
+// 	tag := reference.Tag()
+// 	var suffix string
+// 	if strings.HasPrefix(tag, "sha256:") {
+// 		suffix = "@" + tag
+// 	} else {
+// 		suffix = ":" + tag
+// 	}
+
+// 	newImage := NEW_REGISTRY + "/" + NEW_USERNAME + "/" + reference.Registry() + "." + username + "." + name + suffix
+
+// 	return newImage, nil
+// }
--- src/gh/pkg/convert/convert_test.go
+++ src/gh/pkg/convert/convert_test.go
@@ -35,18 +35,18 @@ func TestConvertToNewImage(t *testing.T) {
 	}{
 		{
 			image:    "mysql",
-			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/docker.io.library.mysql:latest",
+			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/sync:docker.io.library.mysql.latest",
 		}, {
 			image:    "mysql:8.0",
-			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/docker.io.library.mysql:8.0",
+			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/sync:docker.io.library.mysql.8.0",
 		},
 		{
 			image:    "ghcr.io/devcontainers/features/anaconda:1",
-			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/ghcr.io.devcontainers.features/anaconda:1",
+			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/sync:ghcr.io.devcontainers.features.anaconda.1",
 		},
 		{
 			image:    "ubuntu:18.04@sha256:98706f0f213dbd440021993a82d2f70451a73698315370ae8615cc468ac06624",
-			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/docker.io.library.ubuntu@sha256:98706f0f213dbd440021993a82d2f70451a73698315370ae8615cc468ac06624",
+			expected: "registry.cn-hangzhou.aliyuncs.com/117503445-mirror/sync:docker.io.library.ubuntu.sha256.98706f0f213dbd440021993a82d2f70451a73698315370ae8615cc468ac06624",
 		},
 	}
 	for _, c := range cases {
