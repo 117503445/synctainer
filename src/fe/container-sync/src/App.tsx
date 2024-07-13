22efd80b362365dfa2fcaa3822b8cebbdc76edf0
117503445 <t117503445@gmail.com>
2024-07-13 22:53:11 +0800

    ui
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/fe/container-sync/src/App.tsx
+++ src/fe/container-sync/src/App.tsx
@@ -3,9 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
+import React, { useRef } from 'react';
+import IconButton from '@mui/material/IconButton';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import Link from '@mui/material/Link';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const CopyTextField: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const handleCopy = () => {
+    if (inputRef.current) {
+      navigator.clipboard.writeText(inputRef.current.value);
+      setOpenSnackbar(true);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <div>
+    <Box display="flex" alignItems="center">
+      <TextField inputRef={inputRef} variant="outlined" fullWidth />
+      <IconButton onClick={handleCopy} sx={{ height: '100%', marginLeft: '-40px', "&:focus": { outline: 'none' } }}>
+        <FileCopyIcon />
+      </IconButton>
+    </Box>
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={openSnackbar}
+      autoHideDuration={3000}
+      onClose={handleCloseSnackbar}
+    >
+      <MuiAlert onClose={handleCloseSnackbar} severity="success" sx={{ width: '100%' }}>
+        Text copied to clipboard!
+      </MuiAlert>
+    </Snackbar>
+  </div>
+  );
+};
 
 
 const top100Films = [
@@ -14,7 +59,7 @@ const top100Films = [
   { title: 'The Godfather: Part II', year: 1974 },
 ]
 
-const platforms =[
+const platforms = [
   "linux/amd64",
   "linux/arm64",
 ]
@@ -25,6 +70,9 @@ function App() {
   return (
     <>
       <Stack spacing={2} sx={{ width: 300 }}>
+
+        <TextField id="outlined-basic" label="Image" variant="outlined" />
+
         <Autocomplete
           id="platform"
           freeSolo
@@ -32,6 +80,13 @@ function App() {
           options={platforms}
           renderInput={(params) => <TextField {...params} label="Platform" />}
         />
+
+
+
+        <Button variant="contained">Sync</Button>
+
+        <CopyTextField />
+        <Link href="https://github.com/117503445/container-copier/actions/workflows/copy.yml">See Progress</Link>
       </Stack>
 
       {/* <div>
